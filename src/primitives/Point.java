43e69307011ec9226a1bed011d5ec1d586f949d3
Yair Elhasid <yairyair613859@gmail.com>
2024-05-07 18:59:39 +0300

    not working yet, but point half made

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,4 +1,62 @@
 package primitives;
-
+/**
+ * The class implements a point object
+ * @author Sagiv Maoz and Yair Elhasid
+ */
 public class Point {
+    /**
+     * xyz is the coordinate of the point
+     */
+    final protected Double3 xyz;
+    /**
+     * origin coordinate
+     */
+    final public static Point ZERO = new Point(0,0,0);
+
+    /**
+     * full constructor
+     * @param x - x
+     * @param y - y
+     * @param z - z
+     */
+    public Point(double x, double y, double z) {
+        xyz = new Double3(x, y, z);
+    }
+
+    /**
+     * constructor that get a Double3 object
+     * @param xyz - the Double3 object
+     */
+    public Point(Double3 xyz) {
+        this.xyz = xyz;
+    }
+
+    /**
+     * subtract the second point from the first one
+     * @param other the second point
+     * @return the result as a new vector
+     */
+    public Vector subtract(Point other){
+        return new Vector(xyz.subtract(other.xyz));
+    }
+
+    /**
+     * progressing the old point in the direction of the vector
+     * @param other - the other vector
+     * @return new point - the result
+     */
+    public Point add(Vector other){
+        return new Point(xyz.add(other.xyz));
+    }
+
+    /**
+     * calculates the squared distance between 2 points
+     * @param other -the other point
+     * @return the squared distance
+     */
+    public double distanceSquared(Point other){
+        return (xyz.d1 - other.xyz.d1)*(xyz.d1 - other.xyz.d1)
+                + (xyz.d2 - other.xyz.d2) * (xyz.d2 - other.xyz.d2)
+                + (xyz.d3 - other.xyz.d3) * (xyz.d3 - other.xyz.d3);
+    }
 }
