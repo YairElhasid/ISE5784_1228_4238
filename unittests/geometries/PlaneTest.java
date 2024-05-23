eeebbae3f43475b5e63150d62ff0ca9593c7774c
sagivmaoz@gmail.com <sagivmaoz@gmail.com>
2024-05-23 16:32:33 +0300

    unitests - plane final tests

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -18,7 +18,7 @@ class PlaneTest {
         // TC00: checks the calculation of the normal
         assertTrue(new Vector(1 / Math.sqrt(3), 1 / Math.sqrt(3), 1 / Math.sqrt(3)).equals(new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1)).getNormal()) || new Vector(-1 / Math.sqrt(3), -1 / Math.sqrt(3), -1 / Math.sqrt(3)).equals(new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1)).getNormal()) , "failed constructor");
         // TC01: checks that the constructor do not throws an exception on normal case
-        assertDoesNotThrow(() -> new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1)), "throwing exception for some reason");
+        assertDoesNotThrow(() -> new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1)), "throwing exception in full constructor for some reason");
         // TC02: checks that the normal's length is 1
         assertEquals(new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1)).getNormal().length(),1,  "normal's length is not 1");
 
@@ -33,29 +33,25 @@ class PlaneTest {
     @Test
     void testPointVectorConstructor() {
         // ============ Equivalence Partitions Tests ==============
-        // No assert statements to document
-
-        // =============== Boundary Values Tests ==================
-        // No assert statements to document
+        // TC01: just check that the second constructor do not throws an exception
+        assertDoesNotThrow(() -> new Plane(new Point(1, 0, 0), new Vector(1, 1, 1)), "throwing exception in normal constructor for some reason");
+        // TC02: check for correct normalized normal
+        assertTrue(new Vector(1 / Math.sqrt(3), 1 / Math.sqrt(3), 1 / Math.sqrt(3)).equals(new Plane(new Point(1, 0, 0), new Vector(1, 1, 1)).getNormal()) || new Vector(-1 / Math.sqrt(3), -1 / Math.sqrt(3), -1 / Math.sqrt(3)).equals(new Plane(new Point(1, 0, 0), new Vector(1, 1, 1)).getNormal()), "failed normalization on normal constructor");
     }
 
     /** Test method for {@link geometries.Plane#getNormal(Point)}. */
     @Test
     void testGetNormalP() {
         // ============ Equivalence Partitions Tests ==============
-        // No assert statements to document
-
-        // =============== Boundary Values Tests ==================
-        // No assert statements to document
+        // TC01: check the normal returned
+        assertTrue(new Vector(1 / Math.sqrt(3), 1 / Math.sqrt(3), 1 / Math.sqrt(3)).equals(plane.getNormal(new Point(1,0,0))) || new Vector(-1 / Math.sqrt(3), -1 / Math.sqrt(3), -1 / Math.sqrt(3)).equals(plane.getNormal(new Point(1,0,0))) , "wrong normal returned");
     }
 
     /** Test method for {@link geometries.Plane#getNormal()}. */
     @Test
     void testGetNormal() {
         // ============ Equivalence Partitions Tests ==============
-        // No assert statements to document
-
-        // =============== Boundary Values Tests ==================
-        // No assert statements to document
+        // TC01: check the normal returned
+        assertTrue(new Vector(1 / Math.sqrt(3), 1 / Math.sqrt(3), 1 / Math.sqrt(3)).equals(plane.getNormal()) || new Vector(-1 / Math.sqrt(3), -1 / Math.sqrt(3), -1 / Math.sqrt(3)).equals(plane.getNormal()) , "wrong normal returned");
     }
 }
