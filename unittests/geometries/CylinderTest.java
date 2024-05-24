b9f202d0264e92c5a52c76e835e29f183b52c4e7
sagivmaoz@gmail.com <sagivmaoz@gmail.com>
2024-05-24 17:53:38 +0300

    unitests - cylinde fix

--- unittests/geometries/CylinderTest.java
+++ unittests/geometries/CylinderTest.java
@@ -16,16 +16,16 @@ class CylinderTest {
     void getNormal() {
         // ============ Equivalence Partitions Tests ==============
         // TC00: normal at the bottom base of the cylinder
-        assertTrue(new Vector(1, 0, 0).equals(cylinder.getNormal(new Point(2, 3, 7))) || new Vector(-1, 0, 0).equals(cylinder.getNormal(new Point(2, 3, 7))), "failed normal on bottom base");
+        assertTrue(new Vector(0, 0, 1).equals(cylinder.getNormal(new Point(2, 3, 7))) || new Vector(0, 0, -1).equals(cylinder.getNormal(new Point(2, 3, 7))), "failed normal on bottom base");
         // TC01: normal at the top base of the cylinder
-        assertTrue(new Vector(1, 0, 0).equals(cylinder.getNormal(new Point(12, 3, 7))) || new Vector(-1, 0, 0).equals(cylinder.getNormal(new Point(12, 3, 7))), "failed normal on top base");
+        assertTrue(new Vector(0, 0, 1).equals(cylinder.getNormal(new Point(12, 3, 7))) || new Vector(0, 0, -1).equals(cylinder.getNormal(new Point(12, 3, 7))), "failed normal on top base");
         // TC02: normal at the shell of the cylinder
         assertTrue(new Vector(0, 1, 0).equals(cylinder.getNormal(new Point(7, 8, 4))) || new Vector(0, -1, 0).equals(cylinder.getNormal(new Point(7, 8, 4))), "failed normal on shell");
 
         // =============== Boundary Values Tests ==================
         // TC10: normal at the center of the top base of the cylinder
-        assertTrue(new Vector(1, 0, 0).equals(cylinder.getNormal(new Point(12, 3, 4))) || new Vector(-1, 0, 0).equals(cylinder.getNormal(new Point(12, 3, 4))), "failed normal on center of top base");
+        assertTrue(new Vector(0, 0, 1).equals(cylinder.getNormal(new Point(12, 3, 4))) || new Vector(0, 0, -1).equals(cylinder.getNormal(new Point(12, 3, 4))), "failed normal on center of top base");
         // TC11: normal at the center of the bottom base of the cylinder
-        assertTrue(new Vector(1, 0, 0).equals(cylinder.getNormal(new Point(2, 3, 4))) || new Vector(-1, 0, 0).equals(cylinder.getNormal(new Point(2, 3, 4))), "failed normal on center of bottom base");
+        assertTrue(new Vector(0, 0, 1).equals(cylinder.getNormal(new Point(2, 3, 4))) || new Vector(0, 0, -1).equals(cylinder.getNormal(new Point(2, 3, 4))), "failed normal on center of bottom base");
     }
 }
