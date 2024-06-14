9a60d53b9dd400f454422b54ece8f99c41bfccc0
Yair Elhasid <yairyair613859@gmail.com>
2024-06-14 16:29:00 +0300

    stage 5.

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,7 +1,17 @@
 package renderer;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 /**
  * Testing ImageWriter class
  * @author Yair Elhasid and Sagiv Maoz
  */
 public class ImageWriterTest {
+    /** test image writer*/
+    @Test
+    void testImageWriter(){
+
+    }
 }
