e31d3bccf4b273277fd30e8052ed3ab3dc7cacdb
Yair Elhasid <yairyair613859@gmail.com>
2024-06-14 16:53:10 +0300

    stage 5 here we come

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,6 +1,7 @@
 package renderer;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
 
 import java.io.IOException;
 
@@ -12,6 +13,13 @@ public class ImageWriterTest {
     /** test image writer*/
     @Test
     void testImageWriter(){
-
+        ImageWriter imageWriter = new ImageWriter("surprise",800,500);
+        for(int i=0;i< imageWriter.getNx();i++) {
+            for(int j=0;j< imageWriter.getNy();j++){
+                imageWriter.writePixel(i,j, i % 50 == 0 || j % 50 == 0 ?
+                        new Color(100,150,50) : new Color(121,40,230));
+            }
+        }
+        imageWriter.writeToImage();
     }
 }
